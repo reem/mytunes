16ef3706bec4d6a3e341bd2cb92f92399af3942b
Jonathan Reem <jonathan.reem@gmail.com>
2014-04-30 16:20:43 -0700

    Added songQueue instantiation and methods.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,19 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
+  initialize: function () {
+    // Listen for enqueue events, handle them.
+    this.on('end', function () {
+      this.shift();
+      if (this.length) {
+        this.trigger('newSong', this.at(0));
+      }
+    });
+  },  
 
-  initialize: function(){
+  enqueue: function (song) {
+    this.push(song);
+    if (this.length === 1) {
+      this.trigger('newSong', song);
+    }
   }
-
 });
