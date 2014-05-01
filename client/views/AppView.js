8bb5326f4b4c07a2b68aeab1d2791cfa61ae568a
Jonathan Reem <jonathan.reem@gmail.com>
2014-04-30 19:13:12 -0700

    Switched to collection-backed view.

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -3,7 +3,7 @@ var AppView = Backbone.View.extend({
 
   initialize: function(params){
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
-    this.queueView = new SongQueueView({model: this.model.get('songQueue')});
+    this.queueView = new SongQueueView({collection: this.model.get('songQueue')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
 
     // change:currentSong - this is Backbone's way of allowing you to filter events to
